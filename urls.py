a28782e7bdd98bf85770e13664cb2edba95ca149
Cody Wilson <Cheex11@gmail.com>
2016-08-24 14:41:00 -0400

    updated secret to use environment variable.

--- urls.py
+++ urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf.urls import include, url
 from django.contrib import admin
-admin.autodiscover()
+from django.conf.urls.static import  static
+from django.conf import settings
 
-urlpatterns = patterns('',
-    # url(r'^$', 'django_project.views.home', name='home'),
-    url(r'^blog/', include('blog.urls')),
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+]
